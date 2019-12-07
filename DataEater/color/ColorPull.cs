1219b39e581837acc781a93f7cbc3b9dfed049bb
sha99y <prises99@gmail.com>
2019-12-07 15:58:28 +0300

    end color change

--- DataEater/color/ColorPull.cs
+++ DataEater/color/ColorPull.cs
@@ -8,9 +8,8 @@ namespace DataEater
     {
         public ColorPull()
         {
-            using (StreamReader sr = new StreamReader(@"D:\project\c_sharp\DataEater\DataEaterV1.8\DataEater\source\Color.txt"))
+            using (StreamReader sr = new StreamReader(@"D:\project\github\DataEater\DataEater\source\Color.txt"))
             {
-                int count = 0;
                 string line = sr.ReadToEnd();
                 string[] dataColorS = line.Split(new char[] { ' ', '\n' });
                 int[] dataColor = new int[dataColorS.Length];
